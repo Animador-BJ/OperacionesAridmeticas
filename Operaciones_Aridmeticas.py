6708e6009226424ec66a9e9b3235c569e2b6ffa4
jonathanbenitezmayorga <jonathanbenitez@colegioguanena.edu.co>
2024-02-22 16:48:07 -0500

    se agrego el diagrama.png, se actualizo el archivo README, y se agrego el codigo python del programa

---
+++ Operaciones_Aridmeticas.py
@@ -0,0 +1,30 @@
+# input
+
+x = int(input("Digite el valor x : "))
+y = int(input("Digite el Valor y : "))
+
+# processing
+
+print("-----------------------------------")
+
+s = x + y
+r = x - y
+m = x * y
+d = x / y
+de = x // y
+mod = x % y
+p = x ** y
+
+print("-----------------------------------")
+
+# output
+print("-----------------------------------")
+print("Resultados")
+print("la suma de " + str(x)+ " + " + str(y) + " es " + str(s) )
+print("la resta de " + str(x)+ " - " + str(y) + " es " + str(r) )
+print("la multiplicacion de " + str(x)+ " * " + str(y) + " es " + str(m) )
+print("la division de " + str(x)+ " / " + str(y) + " es " + str(d) )
+print("la division Entera de " + str(x)+ " // " + str(y) + " es " + str(de) )
+print("el modulo  de " + str(x)+ " % " + str(y) + " es " + str(mod) )
+print("la potencia de " + str(x)+ " ** " + str(y) + " es " + str(p) )
+print("-----------------------------------")
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# OperacionesAridmeticas
-Programa para desarrollar varias operaciones aridmeticas 
+# Ejercicio N° 3
+
+## Implementar un programapara calcular la suma, resta, multiplicacion, division, division entera, modulo y potencia
+
+---
+# ANALISIS
+
+Variables de entrada (imput)
+
+x & y: Numeros naturales utilisados para sacar el resultado de las operaciones
+
+Variables de proceso y salida (Processing, storage, output)
+
+---
+
+s: Suma
+
+r: Resta
+
+m: Multiplicacion
+
+d: Division
+
+de: Division Entera
+
+mod: Modulo
+
+p: Potencia
+
+---
+# DISEÑO
+
+![Diagrama de Fluji](diagrama.png "Diagrama de flujo")
+
+# CONSTRUCCION
